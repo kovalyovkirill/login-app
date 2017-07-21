f7da09088c2caa7171aff83a75a3cf0f7fab4c4e
Kovalyov Kirill <koval4ik2009@gmail.com>
2017-07-21 17:40:17 +0300

    initial commit

---
+++ src/app/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PureComponent } from 'react'
+
+import { createStore, applyMiddleware } from 'redux'
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <h1>Hello, asddasd@</h1>
+    )
+  }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path'
+import * as webpack from 'webpack'
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './src/static')
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/',
+    contentBase: path.join(__dirname, "./src/static"),
+    compress: true,
+    port: 9000
+  },
+};
